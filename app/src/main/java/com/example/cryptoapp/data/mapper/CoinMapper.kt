8d5cd70ad7817483f4c43dbd8fc6252aac1999e7
Dmitriy Chugunov <yolvissplay@gmail.com>
2023-05-25 16:07:44 +0300

    change mappers and models

--- app/src/main/java/com/example/cryptoapp/data/mapper/CoinMapper.kt
+++ app/src/main/java/com/example/cryptoapp/data/mapper/CoinMapper.kt
@@ -6,6 +6,9 @@ import com.example.cryptoapp.data.network.model.CoinInfoJsonContainerDto
 import com.example.cryptoapp.data.network.model.CoinNamesListDto
 import com.example.cryptoapp.domain.CoinInfo
 import com.google.gson.Gson
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CoinMapper {
 
@@ -17,7 +20,7 @@ class CoinMapper {
         highDay = dto.highDay,
         lowDay = dto.lowDay,
         lastMarket = dto.lastMarket,
-        imageUrl = dto.imageUrl,
+        imageUrl = BASE_IMAGE_URL + dto.imageUrl,
         lastUpdate = dto.lastUpdate
     )
 
@@ -55,7 +58,21 @@ class CoinMapper {
             lowDay = dbModel.lowDay,
             lastMarket = dbModel.lastMarket,
             imageUrl = dbModel.imageUrl,
-            lastUpdate = dbModel.lastUpdate
+            lastUpdate = convertTimestampToTime(dbModel.lastUpdate)
         )
+    }
+
+    fun convertTimestampToTime(timestamp: Long?): String {
+        if (timestamp == null) return ""
+        val stamp = Timestamp(timestamp * 1000)
+        val date = Date(stamp.time)
+        val pattern = "HH:mm:ss"
+        val sdf = SimpleDateFormat(pattern, Locale.getDefault())
+        sdf.timeZone = TimeZone.getDefault()
+        return sdf.format(date)
+    }
+
+    companion object {
+        const val BASE_IMAGE_URL = "https://cryptocompare.com"
     }
 }
--- app/src/main/java/com/example/cryptoapp/domain/CoinInfo.kt
+++ app/src/main/java/com/example/cryptoapp/domain/CoinInfo.kt
@@ -5,11 +5,11 @@ data class CoinInfo(
     val fromSymbol: String,
     val toSymbol: String?,
     val price: String?,
-    val lastUpdate: Long?,
+    val lastUpdate: String,
     val lastVolume: String?,
     val highDay: String?,
     val lowDay: String?,
     val lastMarket: String?,
-    val imageUrl: String?
+    val imageUrl: String
 
 )
